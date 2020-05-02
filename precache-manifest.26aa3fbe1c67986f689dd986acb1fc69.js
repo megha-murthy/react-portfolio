8a46fa774704a0fb744ae8927e5c994240393cff
Megha Murthy <m.megha21@gmail.com>
2020-05-01 17:42:24 -0700

    Updates

---
+++ precache-manifest.26aa3fbe1c67986f689dd986acb1fc69.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc5e6e6beda5d5ba43f17fa1a8f31535",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "9dd66d0bd59b070fe96c",
+    "url": "/react-portfolio/static/css/main.ec4325aa.chunk.css"
+  },
+  {
+    "revision": "ea883619d5e68786c14a",
+    "url": "/react-portfolio/static/js/2.1ef8f375.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.1ef8f375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dd66d0bd59b070fe96c",
+    "url": "/react-portfolio/static/js/main.856dd0c8.chunk.js"
+  },
+  {
+    "revision": "10b51d5b5a56c13e78c7",
+    "url": "/react-portfolio/static/js/runtime-main.fdede8d3.js"
+  },
+  {
+    "revision": "8bdd212691340e0d484782f45bdbb947",
+    "url": "/react-portfolio/static/media/Crime Radar.8bdd2126.png"
+  },
+  {
+    "revision": "285687bafc759e4638ae06d039365d72",
+    "url": "/react-portfolio/static/media/Interior Designing.285687ba.jpg"
+  },
+  {
+    "revision": "dc001cdbec45a79177e35e103729b54f",
+    "url": "/react-portfolio/static/media/Rendezvous.dc001cdb.png"
+  },
+  {
+    "revision": "278dc044e98f183627da7c6795f09adf",
+    "url": "/react-portfolio/static/media/my-passport-photo.278dc044.jpg"
+  }
+]);
